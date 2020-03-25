c724e1eccd93727a2309bd51b3bf2be05e0e971a
Dmitrii Tcimbal <dtsimbal@gmail.com>
2020-03-26 06:34:23 +1100

    Creates topic with terraform

--- docker/provisioning/main.tf
+++ docker/provisioning/main.tf
@@ -1,10 +1,11 @@
 provider "kafka" {
-  bootstrap_servers = ["broker:9092"]
+  bootstrap_servers = ["broker:29092"]
 
   ca_cert     = file("/usr/secrets/snakeoil-ca-1.crt")
   client_cert = file("/usr/secrets/kafkacat-ca1-signed.pem")
   client_key  = file("/usr/secrets/kafkacat-raw-private-key.pem")
   tls_enabled = true
+  skip_tls_verify = true
 }
 
 resource "kafka_topic" "syslog" {

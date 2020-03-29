9c439e2cc09fee0f5168b39acc83d86211488d6e
Dmitrii Tcimbal <dtsimbal@gmail.com>
2020-03-30 06:15:38 +1100

    Fixes the image provisioning

--- docker/provisioning/main.tf
+++ docker/provisioning/main.tf
@@ -8,6 +8,14 @@ provider "kafka" {
   skip_tls_verify = true
 }
 
+provider "kafka-connect" {
+  url = "http://kafka-connect-cp:18083"
+}
+
+//provider "ksql" {
+//
+//}
+
 resource "kafka_topic" "syslog" {
   provider           = kafka
   name               = "syslog"
@@ -20,41 +28,37 @@ resource "kafka_topic" "syslog" {
   }
 }
 
-//provider "kafka_connect" {
-//  url = "http://kafka-connect-cp:18083"
-//}
-//
-//resource "kafka-connect-connector" "jdbc-summary-sink" {
-//  provider                                = kafka_connect
-//  name                                    = "jdbc-summary-sink"
-//  config                                  = {
-//    "name"                                = "jdbc-summary-sink"
-//    "connector.class"                     = "io.confluent.connect.jdbc.JdbcSinkConnector",
-//    "connection.url"                      = "jdbc:postgresql://postgres:5432/kappademo?user=postgres&password=postgres",
-//    "auto.create"                         = true,
-//    "auto.evolve"                         = true,
-//
-//    "insert.mode"                         = "upsert",
-//    "mode"                                = "incrementing",
-//    "table.name.format"                   = "summary",
-//    "topics"                              = "summary",
-//    "numeric.mapping"                     = "best_fit",
-//
-//    "key.converter"                       = "org.apache.kafka.connect.storage.StringConverter",
-//    "key.converter.schemas.enable"        = "false",
-//
-//    "value.converter"                     = "io.confluent.connect.avro.AvroConverter",
-//    "value.converter.schemas.enable"      = "true",
-//    "value.converter.schema.registry.url" = "http://schema-registry:8081",
-//
-//    "pk.mode"                             = "record_value",
-//    "pk.fields"                           = "MERCHANT_ID,PAYMENT_SCHEME",
-//
-//    "dialect.name"                        = "PostgreSqlDatabaseDialect"
+resource "kafka-connect_connector" "jdbc-summary-sink" {
+  provider                                = kafka-connect
+  name                                    = "jdbc-summary-sink"
+  config                                  = {
+    "name"                                = "jdbc-summary-sink"
+    "connector.class"                     = "io.confluent.connect.jdbc.JdbcSinkConnector",
+    "connection.url"                      = "jdbc:postgresql://postgres:5432/kappademo?user=postgres&password=postgres",
+    "auto.create"                         = true,
+    "auto.evolve"                         = true,
+
+    "insert.mode"                         = "upsert",
+    "mode"                                = "incrementing",
+    "table.name.format"                   = "summary",
+    "topics"                              = "summary",
+    "numeric.mapping"                     = "best_fit",
+
+    "key.converter"                       = "org.apache.kafka.connect.storage.StringConverter",
+    "key.converter.schemas.enable"        = "false",
+
+    "value.converter"                     = "io.confluent.connect.avro.AvroConverter",
+    "value.converter.schemas.enable"      = "true",
+    "value.converter.schema.registry.url" = "http://schema-registry:8081",
+
+    "pk.mode"                             = "record_value",
+    "pk.fields"                           = "MERCHANT_ID,PAYMENT_SCHEME",
+
+    "dialect.name"                        = "PostgreSqlDatabaseDialect"
+  }
+
+//  config_sensitive = {
+//  "connection.password" = "this-should-never-appear-unmasked"
 //  }
-//
-////  config_sensitive = {
-////  "connection.password" = "this-should-never-appear-unmasked"
-////  }
-//
-//}
+
+}
